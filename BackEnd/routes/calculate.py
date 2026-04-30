2e1745b0767dde1764e63a32af5918dc89a4172a
eyad amr <eyadamr1644@gmail.com>
2026-04-30 03:36:13 +0300

    finished the forwar_paths

--- BackEnd/routes/calculate.py
+++ BackEnd/routes/calculate.py
@@ -25,8 +25,33 @@ def calculate():
 
 
 def find_forward_paths(g):
-    paths = []
-    # TODO: your implementation
+    paths=[]
+
+    def _find_forward_paths_helper(g,node,visited,path_gain):
+        """
+        DFS helper to find all forward paths from input to output node.
+        - node: current node being visited
+        - visited: list of nodes visited so far (also represents current path)
+        - path_gain: accumulated gain string along the current path
+        """
+        # base case: reached the sink node
+        if node==g.output_node:
+            visited.append(node)
+            paths.append(_build_dict(len(paths)+1,path_gain,list(visited)))
+            return
+
+        # mark current node as visited
+        visited.append(node)
+
+        for edge in g.adj[node]:
+            if edge.to not in visited:
+                # build gain for this edge without modifying parent's gain
+                new_gain=path_gain+"*"+edge.gain if path_gain!= "" else edge.gain
+                _find_forward_paths_helper(g,edge.to,visited,new_gain)
+                # backtrack
+                visited.remove(edge.to)
+
+    _find_forward_paths_helper(g,g.input_node,[],"")
     return paths
 
 
@@ -34,3 +59,9 @@ def find_loops(g):
     loops = []
     # TODO: your implementation
     return loops
+
+
+
+def _build_dict(index,forward_paths_gain,path)->dict:
+    dict_path = {"index": index, "gain": forward_paths_gain, "nodesPath": path}
+    return dict_path
